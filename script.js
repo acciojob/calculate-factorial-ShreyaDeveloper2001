036cbfb9774e506e237a95d378048aaae3fac897
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-05 20:54:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+// Prompt the user to enter a number
+const userInput = prompt("Enter a number:");
+
+// Parse the input string into an integer
+const number = parseInt(userInput);
+
+// Function to calculate the factorial of a number
+function factorial(num) {
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+
+// Calculate the factorial
+const result = factorial(number);
+
+// Display the result in an alert box
+alert(`The factorial of ${number} is ${result}`);
+
